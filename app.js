5564621bcd6c9138d9b7191294e6e390711104f3
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-05-13 23:58:44 +0200

    Removed

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('', function() {
-  menu.classList.toggle('is-active')
-  menuLinks.classList.toggle('active')
-}
+ 
